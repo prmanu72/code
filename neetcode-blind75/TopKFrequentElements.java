79154894c46fcba07efc02471a1db69e8301d567
prashanth4720 <prashanth4720@gmail.com>
2025-09-04 23:27:57 +0530

    top K frequent elements

--- neetcode-blind75/TopKFrequentElements.java
+++ neetcode-blind75/TopKFrequentElements.java
@@ -1,3 +1,4 @@
+// using PriorityQueue
 class Solution {
     public int[] topKFrequent(int[] nums, int k) 
     {
@@ -24,6 +25,39 @@ class Solution {
         {
             int[] ar = pq.poll();
             res[i] = ar[1];
+        }
+        return res;
+    }
+}
+// using bucket sort
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) 
+    {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int i : nums)
+        {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for(int i =0; i< buckets.length; i++)
+        {
+            buckets[i] = new ArrayList<Integer>();
+        }
+
+        for(Map.Entry<Integer, Integer> entry: freq.entrySet())
+        {
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for(int i = buckets.length-1; i > 0 && index < k; i--)
+        {
+            for(int it: buckets[i])
+            {
+                res[index++] = it;
+            }
         }
         return res;
     }
